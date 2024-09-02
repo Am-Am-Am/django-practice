b77028c42e35b4eca5ef3b07ad1a1b593b0be8f3
AmAmAm277 <amur_almukhametov@mail.ru>
2024-09-02 11:50:56 +0500

    fistures add

--- goods/admin.py
+++ goods/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
-from goods.models import Categories
+from goods.models import Categories, Products
 
-admin.site.register(Categories)
+
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
