c895f6c098c1c98a94449cd7c1a112b276ed199a
AmAmAm277 <amur_almukhametov@mail.ru>
2024-09-11 09:49:14 +0500

    Templates for users app added

---
+++ users/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+# Create your views here.
+def login(request):
+    context = {
+        'title': 'Login'
+    }
+    return render(request, 'users/login.html', context)
+
+def registration(request):
+    context = {
+        'title': 'registration'
+    }
+    return render(request, 'users/registration.html', context)
+
+def profile(request):
+    context = {
+        'title': 'profile'
+    }
+    return render(request, 'users/profile.html', context)
+
+def logout(request):
+    context = {
+        'title': 'Logout'
+    }
+    return render(request, '', context)
