b07a3d3b134c6d4590bf08f9d28fe19ed0f145e8
AmAmAm277 <amur_almukhametov@mail.ru>
2024-09-11 16:35:36 +0500

    login req decorator redirect to target page

--- users/views.py
+++ users/views.py
@@ -17,6 +17,10 @@ def login(request):
             if user:
                 auth.login(request, user)
                 messages.success(request, f'{username}, вы вошли в аккаунт')
+
+                if request.POST.get('next', None):
+                    return HttpResponseRedirect(request.POST.get['next'])
+
                 return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserLoginForm()
@@ -61,6 +65,7 @@ def profile(request):
         'form': form
     }
     return render(request, 'users/profile.html', context)
+
 @login_required
 def logout(request):
     messages.success(request, 'Вы вышли из аккаунта')
