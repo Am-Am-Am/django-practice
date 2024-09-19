21bd08231bea2d59523cd0e663782022c4f7eb60
AmAmAm277 <amur_almukhametov@mail.ru>
2024-09-19 09:52:45 +0500

    Remove from cart ajax and json response(js work so-so)

--- carts/views.py
+++ carts/views.py
@@ -10,6 +10,7 @@ from goods.models import Products
 def cart_add(request):
 
     product_id = request.POST.get("product_id")
+
     product = Products.objects.get(id=product_id)
 
     if request.user.is_authenticated:
@@ -23,25 +24,34 @@ def cart_add(request):
         else:
             Cart.objects.create(user=request.user, product=product, quantity=1)
 
-    
+
     user_cart = get_user_carts(request)
     cart_items_html = render_to_string(
         "carts/includes/included_cart.html", {"carts": user_cart}, request=request)
+
     response_data = {
         "message": "Товар добавлен в корзину",
         "cart_items_html": cart_items_html,
     }
+
     return JsonResponse(response_data)
 
 
 def cart_change(request, product_slug):
     ...
 
-def cart_remove(request, cart_id):
+def cart_remove(request):
 
+    cart_id = request.POST.get("cart_id")
     cart = Cart.objects.get(id=cart_id)
+    quantity = cart.quantity
     cart.delete()
-
-    return redirect(request.META['HTTP_REFERER'])
-
-  
+    user_cart = get_user_carts(request)
+    cart_items_html = render_to_string(
+        "carts/includes/included_cart.html", {"carts": user_cart}, request=request)
+    response_data = {
+        "message": "Товар удален",
+        "cart_items_html": cart_items_html,
+        "quantity_deleted": quantity,
+    }
+    return JsonResponse(response_data)
